fc0dee0925789d47b3773b7367ea478e0f2105d6
su yon <suyonsohn@gmail.com>
2018-05-27 16:39:32 -0700

    initialize kittyContract

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -42,6 +42,31 @@ contract KittyInterface {
 }
 
 contract ZombieFeeding is ZombieFactory {
+    // contract NumberInterface {
+        // function getNum(address _myAddress) public view returns (uint);
+    // }
+    
+    // We can use it in a contract as follows:
+
+    // contract MyContract {
+        // address NumberInterfaceAddress = 0xab38... 
+        // ^ The address of the FavoriteNumber contract on Ethereum
+        // NumberInterface numberContract = NumberInterface(NumberInterfaceAddress);
+        // Now `numberContract` is pointing to the other contract
+
+        // function someFunction() public {
+            // Now we can call `getNum` from that contract:
+            // uint num = numberContract.getNum(msg.sender);
+            // ...and do something with `num` here
+        // }
+    // }
+    
+    // In this way, your contract can interact with any other contract on the Ethereum blockchain, as long they expose those functions as public or external.
+    
+    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    // Initialize kittyContract here using `ckAddress` from above
+    KittyInterface kittyContract = KittyInterface(ckAddress);    
+
     // In Solidity, there are two places you can store variables — in storage and in memory.
 
     // Storage refers to variables stored permanently on the blockchain. Memory variables are temporary, and are erased between external function calls to your contract. Think of it like your computer's hard disk vs RAM.
