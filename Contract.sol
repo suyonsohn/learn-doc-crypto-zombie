ff33cf7ab61812e6d7527683756cb78a0d8ea310
su yon <suyonsohn@gmail.com>
2018-05-27 10:45:34 -0700

    mapping zombie id with msg.sender and update zombie owner count

--- Contract.sol
+++ Contract.sol
@@ -54,6 +54,28 @@ contract ZombieFactory {
     function _createZombie(string _name, uint _dna) private {
         // array.push() returns a uint of the new length of the array - and since the first item in an array has index 0, array.push() - 1 will be the index of the zombie we just added. 
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+
+        // In Solidity, there are certain global variables that are available to all functions. One of these is msg.sender, which refers to the address of the person (or smart contract) who called the current function.
+
+        // Note: In Solidity, function execution always needs to start with an external caller. A contract will just sit on the blockchain doing nothing until someone calls one of its functions. So there will always be a msg.sender.
+
+        // Here's an example of using msg.sender and updating a mapping:
+
+        // mapping (address => uint) favoriteNumber;
+
+        // function setMyNumber(uint _myNumber) public {
+            // Update our `favoriteNumber` mapping to store `_myNumber` under `msg.sender`
+        //   favoriteNumber[msg.sender] = _myNumber;
+            // ^ The syntax for storing data in a mapping is just like with arrays
+        // }
+
+        // function whatIsMyNumber() public view returns (uint) {
+            // Retrieve the value stored in the sender's address
+            // Will be `0` if the sender hasn't called `setMyNumber` yet
+        //   return favoriteNumber[msg.sender];
+        // }
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         // fire event
         NewZombie(id, _name, _dna);
     }
