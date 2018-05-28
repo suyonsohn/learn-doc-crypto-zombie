d710e2db40b37b621d30ce9f5babbe89fba2f39e
su yon <suyonsohn@gmail.com>
2018-05-27 17:34:26 -0700

    if zombie eats kitty, the last 2 digits of the zombie dna should be 99

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -107,11 +107,14 @@ contract ZombieFeeding is ZombieFactory {
         // ...if you want to copy the changes back into blockchain storage.
     // }
     // }
-    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
         _targetDna = _targetDna % dnaModulus;
         uint newDna = (myZombie.dna + _targetDna) / 2;
+        if (keccak256(_species) == keccak256("kitty")) {
+            newDna = newDna - newDna % 100 + 99;
+        }        
         _createZombie("NoName", newDna);        
     }    
 
@@ -137,6 +140,6 @@ contract ZombieFeeding is ZombieFactory {
     function feedOnKitty(uint _zombieId, uint _kittyId) public {
         uint kittyDna;
         (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-        feedAndMultiply(_zombieId, kittyDna);
+        feedAndMultiply(_zombieId, kittyDna, "kitty");
     }
 }
