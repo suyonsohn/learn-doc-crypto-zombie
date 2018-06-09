6187685f41c5428b87c9b37be896f3e0762c2e87
su yon <suyonsohn@gmail.com>
2018-06-08 17:25:28 -0700

    add ZombieBattle contract and generate random modulus

---
+++ zombieattack.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.23;
+
+import "./zombiehelper.sol";
+
+contract ZombieBattle is ZombieHelper {
+    // https://ethereum.stackexchange.com/questions/191/how-can-i-securely-generate-a-random-number-in-my-smart-contract
+
+    // In cryptography, a nonce is an arbitrary number that can be used just once. 
+
+    // In Ethereum, when you call a function on a contract, you broadcast it to a node or nodes on the network as a transaction. The nodes on the network then collect a bunch of transactions, try to be the first to solve a computationally-intensive mathematical problem as a "Proof of Work", and then publish that group of transactions along with their Proof of Work (PoW) as a block to the rest of the network.
+    // Once a node has solved the PoW, the other nodes stop trying to solve the PoW, verify that the other node's list of transactions are valid, and then accept the block and move on to trying to solve the next block.
+
+    // Generate random modulus
+    uint randNonce = 0;
+    function randMod(uint _modulus) internal returns(uint) {
+        randNonce++;
+        return uint(keccak256(now, msg.sender, randNonce)) %_modulus;
+    }
+}
