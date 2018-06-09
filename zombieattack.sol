6bc63675f4dbdbfa5cdc8ee37cc7a356a7eab262
su yon <suyonsohn@gmail.com>
2018-06-08 18:39:49 -0700

    build attack function

--- zombieattack.sol
+++ zombieattack.sol
@@ -19,7 +19,9 @@ contract ZombieBattle is ZombieHelper {
         return uint(keccak256(now, msg.sender, randNonce)) %_modulus;
     }
 
-    function attack(uint _zombieId, uint _targetId) external {
-    
-    }    
+    function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
+        Zombie storage myZombie = zombies[_zombieId];
+        Zombie storage enemyZombie = zombies[_targetId];
+        uint rand = randMod(100);
+    }
 }
